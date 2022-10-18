bcbb9c6ff070d2bb6171b89b52ea766078d9271a
Samigirum <samiabge23@gmail.com>
2022-10-18 17:26:21 +0300

    Create 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Getting data from API placeholder."""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    """Gets API endpoint, then identify a user to display completed task info"""
+    endpoint = "https://jsonplaceholder.typicode.com/"
+    userId = sys.argv[1]
+    user = requests.get(endpoint + 'users/{}'.format(userId)).json()
+    todo = requests.get(endpoint + 'todos?userId={}'.format(userId)).json()
+    completed = []
+
+    for task in todo:
+        if task.get("completed"):
+            completed.append(task.get("title"))
+    print("Employee {} is done with task({}/{}):"
+          .format(user.get('name'), len(completed), len(todo)))
+    for task in completed:
+        print('\t', task)
